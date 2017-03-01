41faa5f4a336b3d0ec0c986c7bed829515fcd22a
xuliaP <paukov2967@gmail.com>
2017-03-01 22:59:26 +0300

    remove some migration

--- db/migrate/20160405092854_add_image_to_purchases.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToPurchases < ActiveRecord::Migration
-  def change
-    add_attachment :purchases, :image  
-  end
-end
--- db/migrate/20160407072856_create_cities.rb
+++
@@ -1,9 +0,0 @@
-class CreateCities < ActiveRecord::Migration
-  def change
-    create_table :cities do |t|
-      t.string :name
-    end
-
-    add_reference :purchases, :city, index: true, foreign_key: true
-  end
-end
